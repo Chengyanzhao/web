02b38c031ecc415c37cd1906e9d35773c53711d5
Chengyanzhao <451354@qq.com>
2017-02-08 11:24:34 +0800

    修改：figlet风格

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ platformBrowserDynamic().bootstrapModule(AppModule);
 var figlet = require('figlet');
 figlet.defaults({ fontPath: './assets/fonts' });
 figlet.text('darkclouds.top\r\n451354@qq.com', {
-  font: 'big',
+  font: 'univers',
   horizontalLayout: 'default',
   verticalLayout: 'default'
 }, function (err, data) {
